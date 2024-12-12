37c431d656dc13b37b5d09ac88759aba139db911
salmanumar5 <umarsalman555@gmail.com>
2024-12-12 22:14:22 +0530

    env and video bug

--- src/Components/Home/Landing Page/HeroSection.js
+++ src/Components/Home/Landing Page/HeroSection.js
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import video1 from "../../../assets/video1.mp4";
-import video2 from "../../../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
@@ -13,8 +11,8 @@ const HeroSection = () => {
         </span>
       </h1>
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-      Collaborate effectively with your peers and manage tasks effortlessly using our intuitive group task management tools. Assign
-      roles, track progress, and stay connected in real-time to turn teamwork into seamless success—get started today and transform your productivity!
+        Collaborate effectively with your peers and manage tasks effortlessly using our intuitive group task management tools. Assign
+        roles, track progress, and stay connected in real-time to turn teamwork into seamless success—get started today and transform your productivity!
       </p>
       <div className="flex justify-center my-10">
         <Link
@@ -27,17 +25,18 @@ const HeroSection = () => {
           Documentation
         </Link>
       </div>
-      <div className="flex mt-10 justify-center">
-        <video
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-violet-700 shadow-sm shadow-violet-400 mx-2 my-4"
-        >
-          <source src={video1} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <video
+      <div className="flex h-80 gap-10 mt-10 justify-center overflow-hidden">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/task-mate-c37c5.appspot.com/o/landing-page%2FScreenshot%202024-08-27%20at%208.55.47%E2%80%AFPM.png?alt=media&token=e466097e-aef7-422c-9a80-ec1a04687d70"
+          alt="error"
+          className="h-auto w-full object-cover rounded-md transition-transform duration-300 hover:scale-110"
+        />
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/task-mate-c37c5.appspot.com/o/landing-page%2FScreenshot%202024-08-27%20at%208.55.47%E2%80%AFPM.png?alt=media&token=e466097e-aef7-422c-9a80-ec1a04687d70"
+          alt="error"
+          className="h-auto w-full object-cover rounded-md transition-transform duration-300 hover:scale-110"
+        /> 
+        {/* <video
           autoPlay
           loop
           muted
@@ -45,7 +44,7 @@ const HeroSection = () => {
         >
           <source src={video2} type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </video> */}
       </div>
     </div>
   );
--- src/Utils/Firebase.js
+++ src/Utils/Firebase.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhGnJT1_DL2-OCGabRMaW76N-IFzOaNAk",
-  authDomain: "task-mate-c37c5.firebaseapp.com",
-  projectId: "task-mate-c37c5",
-  storageBucket: "task-mate-c37c5.appspot.com",
-  messagingSenderId: "249531270454",
-  appId: "1:249531270454:web:86a2a507e08b5aec751f97",
-  measurementId: "G-NDY6FMX4MY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
