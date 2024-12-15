5b328f1cb3140a064eab9f0bebffa934eb6ffaa7
salmanumar5 <umarsalman555@gmail.com>
2024-12-15 05:41:37 +0530

    api end point

--- src/Constants/ApiEndPoints.js
+++ src/Constants/ApiEndPoints.js
@@ -1,4 +1,4 @@
-export const BaseUrl = 'http://localhost:5001/api/'
+export const BaseUrl = 'https://task-mate-backend.onrender.com/api/'
 
 export const userLogin = BaseUrl + 'users/login'
 export const userRegister = BaseUrl + 'users/register'
